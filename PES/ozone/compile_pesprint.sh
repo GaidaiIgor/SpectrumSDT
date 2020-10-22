7f1bd38a9eee50ea93cf673b2b414baa60c25032
Igor Gayday <gaidai92@gmail.com>
2020-10-22 17:55:45 -0500

    Fixes ozone PES compilation issues

--- PES/ozone/compile_pesprint.sh
+++ PES/ozone/compile_pesprint.sh
@@ -1,4 +1,5 @@
 DAWES_SRC=../dawes/src/*
-mpifort -c -g ../dawes/src/path_resolution.f90
-mpifort -c -g ../dawes/src/splin.f90
+mpifort -c ../dawes/src/path_resolution.f90 -g -ffree-line-length-0
+mpifort -c ../dawes/src/splin.f90 -g -ffree-line-length-0
+mpifort -c ../dawes/src/dynamic_parameters_3D.f90 -g -ffree-line-length-0
 mpifort ../pesprint.f90 ${DAWES_SRC} -ffree-line-length-0 -g -w -o pesprint
