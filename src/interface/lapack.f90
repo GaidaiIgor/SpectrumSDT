7bf5fd1ea24a42658a2247ff5b9e3c26029145e0
Igor Gayday <gaidai92@gmail.com>
2022-01-24 19:09:27 -0800

    Adds lapack interfaces to LU-factorization and matrix inversion

--- src/interface/lapack.f90
+++ src/interface/lapack.f90
@@ -13,7 +13,7 @@ contains
 !-------------------------------------------------------------------------------------------------------------------------------------------
   subroutine check_info(info)
     integer, intent(in) :: info
-    if (info == 0) then
+    if (info /= 0) then
       print *, 'Error: lapack has failed, info =', info
       stop
     end if
@@ -21,17 +21,16 @@ contains
 
 !-------------------------------------------------------------------------------------------------------------------------------------------
 ! Provides a more convenient interface to dsyev subroutine.
-! matrix - real symmetric matrix, whose eigenpairs are to be found. Contains eigenvectors on exit.
-! eivals - eigenvalues of matrix, in ascending order.
+! matrix - real square symmetric matrix, whose eigenpairs are to be found. Contains eigenvectors on exit.
+! eivals - eigenvalues of matrix, in ascending order. Size has be to equal to size(matrix, 1)
 !-------------------------------------------------------------------------------------------------------------------------------------------
-  subroutine lapack_eigensolver_real(matrix, eivals)
+  subroutine lapack_eigensolver_real_plain(matrix, eivals)
     real(real64), intent(inout) :: matrix(:, :)
-    real(real64), allocatable, intent(out) :: eivals(:)
+    real(real64), intent(out) :: eivals(:)
     integer :: work_size, info
     real(real64) :: work_size_query(1)
     real(real64), allocatable :: work(:)
 
-    allocate(eivals(size(matrix, 1)))
     ! Trivial case: return empty vector if empty matrix is given
     if (size(eivals) == 0) then
       return
@@ -47,6 +46,17 @@ contains
     allocate(work(work_size))
     call dsyev('V', 'U', size(matrix, 1), matrix, size(matrix, 1), eivals, work, work_size, info)
     call check_info(info)
+  end subroutine
+
+!-------------------------------------------------------------------------------------------------------------------------------------------
+! Allocatable version of the main subroutine.
+!-------------------------------------------------------------------------------------------------------------------------------------------
+  subroutine lapack_eigensolver_real(matrix, eivals)
+    real(real64), intent(inout) :: matrix(:, :)
+    real(real64), allocatable, intent(out) :: eivals(:)
+
+    allocate(eivals(size(matrix, 1)))
+    call lapack_eigensolver_real_plain(matrix, eivals)
   end subroutine
 
 !-------------------------------------------------------------------------------------------------------------------------------------------
@@ -81,5 +91,44 @@ contains
     call zheev('V', 'U', size(matrix, 1), matrix, size(matrix, 1), eivals, work, work_size, rwork, info)
     call check_info(info)
   end subroutine
+
+!-------------------------------------------------------------------------------------------------------------------------------------------
+! Calculates LU-factorization of a matrix (dgetrf). Both L and U matrices are written into a single matrix, ignoring unit diagonal of L.
+!-------------------------------------------------------------------------------------------------------------------------------------------
+  subroutine lapack_lu_factorization(matrix, lu_factors, pivot_indices)
+    real(real64), intent(in) :: matrix(:, :)
+    real(real64), allocatable, intent(out) :: lu_factors(:, :)
+    integer, allocatable, intent(out) :: pivot_indices(:)
+    integer :: info
+
+    lu_factors = matrix
+    allocate(pivot_indices(min(size(lu_factors, 1), size(lu_factors, 2))))
+    call dgetrf(size(lu_factors, 1), size(lu_factors, 2), lu_factors, size(lu_factors, 1), pivot_indices, info)
+    call check_info(info)
+  end subroutine
+
+!-------------------------------------------------------------------------------------------------------------------------------------------
+! Calculates inverse of a matrix (dgetri).
+!-------------------------------------------------------------------------------------------------------------------------------------------
+  function lapack_matrix_inverse(matrix) result(inverse)
+    real(real64), intent(in) :: matrix(:, :)
+    real(real64), allocatable :: inverse(:, :)
+    integer :: work_size, info
+    integer, allocatable :: pivot_indices(:)
+    real(real64) :: work_size_query(1)
+    real(real64), allocatable :: work(:)
+
+    call lapack_lu_factorization(matrix, inverse, pivot_indices)
+    ! Work size query
+    work_size = -1
+    call dgetri(size(inverse, 1), inverse, size(inverse, 1), pivot_indices, work_size_query, work_size, info)
+    call check_info(info)
+
+    ! Actual solution
+    work_size = int(work_size_query(1))
+    allocate(work(work_size))
+    call dgetri(size(inverse, 1), inverse, size(inverse, 1), pivot_indices, work, work_size, info)
+    call check_info(info)
+  end function
 
 end module
